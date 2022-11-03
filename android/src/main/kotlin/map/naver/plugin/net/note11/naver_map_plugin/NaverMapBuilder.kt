a9a97ee10bf410ce2e5b5368401211d115c3a593
robofrien@gmail.com <102004343+ROBOFRIEN@users.noreply.github.com>
2022-11-03 20:09:35 +0900

    compass가 IOS만 나와 compassEnable을 강제 false시킴 직접 만들어 쓰는 방향으로 접근할 예정

--- android/src/main/kotlin/map/naver/plugin/net/note11/naver_map_plugin/NaverMapBuilder.kt
+++ android/src/main/kotlin/map/naver/plugin/net/note11/naver_map_plugin/NaverMapBuilder.kt
@@ -132,7 +132,7 @@ class NaverMapBuilder : NaverMapOptionSink {
     override fun setLocationButtonEnable(locationButtonEnable: Boolean) {
         options.indoorLevelPickerEnabled(true)
             .zoomControlEnabled(false)
-            .compassEnabled(true)
+            .compassEnabled(false)
         options.locationButtonEnabled(locationButtonEnable)
     }
 
